429aaf4f9b049af5e1f7cb7253f8f91ba46726cc
nishnareddy1 <aletinishna0723@gmail.com>
2020-09-04 07:23:22 -0700

    typing animation in the header

---
+++ js/text.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#typed", {
+    strings: [
+        "Learner",
+        "Software Developer",
+        "Programmer"
+    ],
+    fadeOut: true,
+    typeSpeed: 70,
+    backSpeed: 70,
+    loop: true
+})
